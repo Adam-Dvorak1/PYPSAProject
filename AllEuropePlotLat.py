35ba042db9eda5f3f70dea67a808717166a9091e
Adam-Dvorak1 <92300992+Adam-Dvorak1@users.noreply.github.com>
2023-01-18 11:55:10 +0100

    Adding the correct Europe map plot from the NoGasRussia onedrive folder

--- AllEuropePlotLat.py
+++ AllEuropePlotLat.py
@@ -4,26 +4,35 @@ Created on Mon Mar  7 14:02:23 2022
 
 @author: au485969
 
-Modified 24 May au699305
+@author: au699305
+
 """
+
+'''This plot was made four times: for scenarios between sectors/no sectors, transmission/no transmission'''
 import matplotlib
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-#from mpl_toolkits.basemap import Basemap
+import glob
+from mpl_toolkits.basemap import Basemap
 from matplotlib.patches import Polygon
 from matplotlib.collections import PatchCollection
 from matplotlib import cm
-import geopandas
 import warnings
 warnings.filterwarnings("ignore")
-import os.path
 
 arrows = False
 case = 'inland demand'
 
+fs = 22
 
+cmap = cm.get_cmap('terrain', 20)  # matplotlib color palette name, n colors
+for i in range(cmap.N):
+    rgb = cmap(i)[:3]  # will return rgba, we take only first 3 so we get rgb
+
+colpal1 = [matplotlib.colors.rgb2hex(cmap(i)[:3]) for i in np.arange(12)]
+colpal2 = [matplotlib.colors.rgb2hex(cmap(i)[:3]) for i in np.arange(12)+12]
 
 # cmap = cm.get_cmap('terrain', 12)  # matplotlib color palette name, n colors
 # for i in range(cmap.N):
@@ -37,7 +46,8 @@ case = 'inland demand'
 
 # colpal2 = [matplotlib.colors.rgb2hex(cmap(i)[:3]) for i in np.arange(12)]
 
-
+#What is the point of the 3code
+#To draw a bound
 Europe_ia3 = {  
             'Albania':'ALB',
             'Austria':'AUT',
@@ -75,9 +85,9 @@ Europe_ia3 = {
             }
 
 cdict = {'Bosnia_Herzegovina':'Bosnia and Herzegovina',
-         'Czech_Republic': 'Czechia',
+         'Czech_Republic': 'Czech Republic',
          'United_Kingdom':'United Kingdom',
-         'Macedonia':'North Macedonia'}
+         'Macedonia': 'Macedonia [FYROM]'}
 
 # import_relative = pd.read_csv('../data/Import_percentages.csv',index_col=0)
 
@@ -95,8 +105,19 @@ cdict = {'Bosnia_Herzegovina':'Bosnia and Herzegovina',
 country_coord = pd.read_csv('Countries_lat_lon.csv',
                             sep=';',header=None) # Country center coordinates
 country_coord.columns = ['Code','Lat','Lon','Country']
-country_coord.set_index('Country',inplace=True)
+country_coord.set_index('Country',inplace=True)#Setting index to country
 
+#print(country_coord)
+# for key in Europe_ia3.keys():
+#     if "shapefiles/gadm36_"+ Europe_ia3[key] + "_1.shp" in glob.glob("shapefiles/*"):
+#         pass
+#     else:
+#         print ( Europe_ia3[key])
+
+
+
+
+#corrections to coordinate, maybe
 country_coord.loc['Italy'] = ['IT',41.4,15]
 country_coord.loc['Denmark'] = ['DK',56.3365,10]
 country_coord.loc['Portugal'] = ['PT',39.3999,-7]
@@ -111,33 +132,77 @@ country_coord.loc['Serbia'] = ['RS',43.5,21]
 country_coord.loc['Greece'] = ['GR',39.0742,22.5]
 country_coord.loc['Croatia'] = ['HR',45.1,16.4]
 
+
+#filepath = '../data/0.25_gen_and_lat_no_sectors_no_transmission.csv'
+#filepath = '../data/0.25_gen_and_lat_no_sectors_yes_transmission.csv'
+#filepath = '../data/0.25_gen_and_lat_yes_sectors_no_transmission.csv'
+filepath = '../data/0.25_gen_and_lat_yes_sectors_yes_transmission.csv'
+
+gen_latdf = pd.read_csv(filepath, index_col = 0)
+
+# gen_latdf = pd.read_csv('../data/0.25_gen_and_lat_no_sectors_yes_transmission.csv', index_col = 0)
+# gen_latdf = pd.read_csv('../data/0.25_gen_and_lat_yes_sectors_no_transmission.csv', index_col = 0) #For this one, we want to include the color bar
+# gen_latdf = pd.read_csv('../data/0.25_gen_and_lat_yes_sectors_yes_transmission.csv', index_col = 0)
+gen_latdf = gen_latdf[gen_latdf['carrier'] == 'solar']
+#print(gen_latdf)
+gen_latdf.set_index('name_y', inplace = True)#For some reason I have two "name"s lol
+
 # max_val = gas_df['gas_consumption_2019_TWh'].max()
 # max_val = gas_df['gas_imports_2019_TWh'].max()
+# max_val = gas_df['gas_' + case + '_2019_TWh'].max()
+max_val = 1 #We want the maximum value to be 1
 
+#Not necessary
+# max_val_idx = gas_df['gas_' + case + '_2019_TWh'].idxmax()
 
-
-fig, ax = plt.subplots() # Initialise figure
-
+# norm=plt.Normalize(-10,gas_df.max().perc_of_pe)
+#gas_df.max().perc_of_pe
+norm = plt.Normalize(0, 1)
+cmap = cm.get_cmap('Reds', 12)
+cmap.set_under('0.8')
+fig, ax = plt.subplots(1,1,figsize=(10,20)) # Initialise figure
+m_plot = Basemap(width=11500000/2.9,height=9000000/2.2,projection='laea',
+                  resolution='i',lat_0=54.5,lon_0=9.5,ax=ax)
+#m_plot.drawcountries()
 for c in Europe_ia3.keys():
-
+    if c in ['Bosnia_Herzegovina', "Macedonia"]:
+        CC_plot = country_coord.loc[cdict[c]]
+        gas_rel = gen_latdf.loc[cdict[c]].solarfrac
+    elif c  == 'United_Kingdom':
+        CC_plot = country_coord.loc[cdict[c]]
+        # gas_abs = gas_df.loc[cdict[c]].T['gas_' + case + '_2019_TWh'] #This line is the amount of gas
+        gas_rel = gen_latdf.loc[cdict[c]].solarfrac#Finds the row with % coming from Russia
+    # elif c in ['Albania', "Switzerland", "Montenegro", "Norway", 'Serbia']:
+    #     CC_plot = country_coord.loc[c]
+    #     gas_rel = -1
+    elif c == 'Czech_Republic':
+        CC_plot = country_coord.loc['Czech Republic']
+        # gas_abs = gas_df.loc[cdict[c]].T['gas_' + case + '_2019_TWh']
+        gas_rel = gen_latdf.loc[cdict[c]].solarfrac
+    else:
+        CC_plot = country_coord.loc[c]
+        # gas_abs = gas_df.loc[c].T['gas_' + case + '_2019_TWh']
+        gas_rel = gen_latdf.loc[c].solarfrac
     
     #Shape files: https://www.gadm.org/download_country_v3.html
-
-    mycountry = geopandas.read_file('shapefiles/gadm36_' + Europe_ia3[c] + '_0.shp')
-    mycountry = mycountry.to_crs("EPSG:3395")
-    mycountry.boundary.plot(ax = ax, color = "black")
-
-
+    m_plot.readshapefile('shapefiles/gadm36_' + Europe_ia3[c] + '_0',c,drawbounds=True,linewidth = 0.1,color='k')
+    patches = []
+    value = gas_rel
+    for info, shape in zip(eval('m_plot.' + c + '_info'), eval('m_plot.' + c)):
+        patches.append(Polygon(np.array(shape), True))
+    patch1=ax.add_collection(PatchCollection(patches, facecolor= cmap(norm(value))))
     
     # ax.annotate(CC_plot.Code,xy=m_plot(np.array(CC_plot.Lon.item()-1), np.array(CC_plot.Lat.item())),color='k', zorder=13)
-    # x,y=m_plot(np.array(CC_plot.Lon.item()-1), np.array(CC_plot.Lat.item()))
-    # m_plot.plot(x,y,"o",markersize=gas_abs/max_val*30,color='magenta',zorder=100)
+    x,y=m_plot(np.array(CC_plot.Lon.item()-1), np.array(CC_plot.Lat.item()))
+    
+    #This plots the circles of gas
+    #m_plot.plot(x,y,"o",markersize=gas_abs/max_val*30,color='magenta',zorder=100)
 
-# xlim = ax.get_xlim()[1] - ax.get_xlim()[0]
-# ylim = ax.get_ylim()[1] - ax.get_ylim()[0]
+xlim = ax.get_xlim()[1] - ax.get_xlim()[0]
+ylim = ax.get_ylim()[1] - ax.get_ylim()[0]
 
-#plt.show()
-# # ax.text(0.02*xlim,0.95*ylim,'2019 EU-28+NO+CH gas ' + case,fontsize=fs,fontweight='bold')
+
+# ax.text(0.02*xlim,0.95*ylim,'2019 EU-28+NO+CH gas ' + case,fontsize=fs,fontweight='bold')
 # ax.text(0.02*xlim,0.95*ylim,'2019 gas demand',fontsize=fs,fontweight='bold')
 # ax.plot(0.05*xlim,0.9*ylim,'o',markersize=900/max_val*30,color='magenta')
 # # ax.text(0.1*xlim,0.9*ylim,str(round(max_val)) + ' TWh',fontsize=fs)
@@ -158,13 +223,18 @@ for c in Europe_ia3.keys():
 # ax.text(0.88*xlim,0.9*ylim,'40 %',fontsize=fs)
 # ax.text(0.88*xlim,0.835*ylim,'20 %',fontsize=fs)
 
-# cb_ax = fig.add_axes([0.85,0.295,0.02,0.21])
-# norm = mpl.colors.Normalize(vmin=-10, vmax=gas_df['perc_of_pe'].max())    
-# cb1 = mpl.colorbar.ColorbarBase(cb_ax,orientation='vertical', cmap=cmap,norm=norm,boundaries=np.arange(0,gas_df.max().perc_of_pe)) #,ticks=bounds, boundaries=bounds) #ticks=[0.15,0.25,0.48,0.90])
-# cb1.set_label('Gas % of total energy demand',zorder=10,fontsize=fs)
-# cb1.ax.tick_params(labelsize=fs)
+#Comment out for most
 
-# #%% Pie chart - import
+cb_ax = fig.add_axes([0.85,0.295,0.02,0.21])
+# norm = mpl.colors.Normalize(vmin=-10, vmax=gas_df['perc_of_pe'].max())    
+norm = mpl.colors.Normalize(vmin=0, vmax=1)    
+bounds = [0, 0.2, 0.4, 0.6, 0.8, 1]
+cb1 = mpl.colorbar.ColorbarBase(cb_ax,orientation='vertical', cmap=cmap,norm=norm,boundaries=bounds) #,ticks=bounds, boundaries=bounds) #ticks=[0.15,0.25,0.48,0.90])
+cb1.set_label('Fraction of electricity from local solar',zorder=10,fontsize=fs)
+cb1.ax.tick_params(labelsize=fs)
+
+#I do not think I need this section
+#%% Pie chart - import
 # pc_imp_ax = fig.add_axes([1.25,0.49,0.35,0.2])
 # pc_imp_ax.pie(import_relative['imp'].values,labels=import_relative.index,colors=colpal1[0:len(import_relative['imp'].values)],startangle = 90,textprops={'fontsize': 19})
 
@@ -186,51 +256,16 @@ ax.spines['left'].set_visible(False)
 
 #%% Arrows
 
-# if arrows == True:
-#     max_width = 0.05
-    
-#     # From Russia
-#     width = max_width #  38 %
-#     lon = 28
-#     lat = 53
-#     x,y=m_plot(lon,lat)
-#     u,v=-2,-1.5
-#     m_plot.quiver(x,y,u,v,width=width,headlength=0.5,headaxislength=0.5,headwidth=1.5)
-#     ax.text(1.01*x,y,'Russia',fontsize=fs)
-    
-#     # From Algeria 
-#     width = max_width*(7/38) # 7 %
-#     lon = 4
-#     lat = 36
-#     x,y=m_plot(lon,lat)
-#     u,v=0,1
-#     m_plot.quiver(x,y,u,v,width=width,headlength=0.5,headaxislength=0.5,headwidth=2)
-#     ax.text(x*1.05,y*1.2,'Algeria',fontsize=fs)
-    
-#     # Own production
-#     width = max_width*(29/38) # 18 % from Norway + 5 % from Netherlands + 6 % from other European
-#     lon = 0.3
-#     lat = 62
-#     x,y=m_plot(lon,lat)
-#     u,v=0.6,-1.9
-#     m_plot.quiver(x,y,u,v,width=width,headlength=0.5,headaxislength=0.5,headwidth=1.5)
-#     ax.text(0.85*x,1.065*y,'Own',fontsize=fs)
-#     ax.text(0.75*x,1.02*y,'production',fontsize=fs)
-    
-#     # Other non-Europe
-#     width = max_width*((17 + 7)/38) # 17 % + 7 % not specified
-#     lon = -8
-#     lat = 47
-#     x,y=m_plot(lon,lat)
-#     u,v=1,0
-#     m_plot.quiver(x,y,u,v,width=width,headlength=0.5,headaxislength=0.5,headwidth=2)
-#     ax.text(0.1*x,y*0.95,'Other',fontsize=fs)
-#     ax.text(0.1*x,y*0.86,'non-European',fontsize=fs)
 
-#     #fig.savefig('../figures/Gas_' + case + '_map_with_arrowstest.jpeg',format='jpeg',dpi=300,bbox_inches='tight')  
+opts = filepath.split("_")
 
-# #else:
-    #fig.savefig('../figures/Gas_' + case + '_map_wo_arrowstest.jpeg',format='jpeg',dpi=300,bbox_inches='tight')  
+sectors = opts[opts.index('sectors')-1]
+
+transmission = opts[opts.index('transmission.csv')-1]
+
+
+
+fig.savefig('../figures/Adamnew'+ sectors + 'sec' + transmission + 'trans.png',format='png',bbox_inches='tight')  
 
 plt.show()
 # fig.savefig('../figures/Gas_imports_map_wo_arrows.jpeg',format='jpeg',dpi=300,bbox_inches='tight')  
